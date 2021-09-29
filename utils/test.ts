91ed45d590c4964e6b31faa59d637be9fe6f58d1
7wataaa <nn.contact.n@gmail.com>
2021-09-29 15:41:47 +0900

    test: リフレッシュトークンのテストを実装

--- utils/test.ts
+++ utils/test.ts
@@ -2,7 +2,7 @@ import crypto from 'crypto';
 import dotenv from 'dotenv';
 import request from 'supertest';
 import { app } from '../app';
-import { createLoginToken } from '../model/createToken';
+import { createLoginToken, createRefreshToken } from '../model/createToken';
 import { prisma } from '../prismaClient';
 import { resetDB } from './resetDB';
 dotenv.config();
@@ -15,7 +15,7 @@ describe('/app.ts', () => {
   });
 });
 
-const existLoginTokenBuffer = crypto.randomBytes(48);
+const existTokenBuffer = crypto.randomBytes(48);
 
 describe('/model/createToken.ts', () => {
   beforeAll(async () => {
@@ -42,23 +42,23 @@ describe('/model/createToken.ts', () => {
     const CryptoRandomBytesSpy = jest.spyOn(crypto, 'randomBytes');
 
     CryptoRandomBytesSpy.mockImplementationOnce((num: number) => {
-      return existLoginTokenBuffer;
+      return existTokenBuffer;
     });
 
-    const existLoginToken = existLoginTokenBuffer
+    const existLoginToken = existTokenBuffer
       .toString('base64')
       .substring(0, 48);
 
     await prisma.user.create({
       data: {
         username: 'testuser',
-        email: 'test@example.com',
+        email: 'logintokentest@example.com',
         hashedPassword: '',
         loginToken: existLoginToken,
         loginTokenExpirationAt: new Date(new Date().getTime() + 30 * 60 * 1000),
         refreshToken: '',
         refreshTokenExpirationAt: new Date(
-          new Date().setMonth(new Date().getMonth() + 30)
+          new Date().getTime() + 30 * 24 * 60 * 60 * 1000
         ),
       },
     });
@@ -67,9 +67,66 @@ describe('/model/createToken.ts', () => {
 
     const { loginToken } = await createLoginToken();
 
-    console.log(`${loginToken}, ${existLoginToken}`);
-
     expect(loginToken).not.toBe(existLoginToken);
+
+    CryptoRandomBytesSpy.mockRestore();
+  });
+
+  test('リフレッシュトークンが適切に生成されるか', async () => {
+    const { refreshToken, refreshTokenExpirationAt } =
+      await createRefreshToken();
+
+    expect(refreshToken).toHaveLength(48);
+    expect(refreshTokenExpirationAt).toMatch(
+      /Z|[+-](0\d|1[012])(:?[012345]\d)?/
+    );
+
+    const refreshTokenExpirationDate = new Date(refreshTokenExpirationAt);
+
+    // 前と後の差がミリ秒で-2日+6ヶ月以上～6ヶ月以下だとOK
+
+    const sixMonthAsMillisecond = 15778800000;
+    const sixMonthMinusTwoDateAsMillisecond = sixMonthAsMillisecond - 172800000;
+
+    const dateDifference =
+      refreshTokenExpirationDate.getTime() - new Date().getTime();
+
+    expect(dateDifference).toBeLessThanOrEqual(sixMonthAsMillisecond);
+    expect(dateDifference).toBeGreaterThanOrEqual(
+      sixMonthMinusTwoDateAsMillisecond
+    );
+  });
+
+  test('リフレッシュトークンがかぶった場合に再生成されるか', async () => {
+    const CryptoRandomBytesSpy = jest.spyOn(crypto, 'randomBytes');
+
+    CryptoRandomBytesSpy.mockImplementationOnce((num: number) => {
+      return existTokenBuffer;
+    });
+
+    const existRefreshToken = existTokenBuffer
+      .toString('base64')
+      .substring(0, 48);
+
+    await prisma.user.create({
+      data: {
+        username: 'testuser',
+        email: 'refreshtokentest@example.com',
+        hashedPassword: '',
+        loginToken: '',
+        loginTokenExpirationAt: new Date(new Date().getTime() + 30 * 60 * 1000),
+        refreshToken: existRefreshToken,
+        refreshTokenExpirationAt: new Date(
+          new Date().getTime() + 30 * 24 * 60 * 60 * 1000
+        ),
+      },
+    });
+
+    const { createRefreshToken } = await import('../model/createToken');
+
+    const { refreshToken } = await createRefreshToken();
+
+    expect(refreshToken).not.toBe(existRefreshToken);
 
     CryptoRandomBytesSpy.mockRestore();
   });
