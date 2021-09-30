2fba53474350278a2bc02c68054d36f1a6d4c99c
7wataaa <nn.contact.n@gmail.com>
2021-09-30 17:07:44 +0900

    fix: test.shを修正

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-npm -v
-
-rm -rf node_modules
-
-yarn install
-
 env DATABASE_URL="postgresql://postgres:postgres@db:5432/test" npx prisma migrate reset --force && env DATABASE_URL="postgresql://postgres:postgres@db:5432/test" npx jest --forceExit --detectOpenHandles
