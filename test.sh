7019099e6e1b25888423dec57d61a27080cbc091
7wataaa <nn.contact.n@gmail.com>
2021-10-03 01:17:50 +0900

    add: github actionsへの対応

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-env DATABASE_URL="postgresql://postgres:postgres@db:5432/test" npx prisma migrate reset --force && env DATABASE_URL="postgresql://postgres:postgres@db:5432/test" npx jest --forceExit --detectOpenHandles
