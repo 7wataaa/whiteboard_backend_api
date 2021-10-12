0807c5c01a250ca01c721627cfa56f616ee191ca
nashi <nn.contact.n@gmail.com>
2021-10-12 15:10:21 +0900

    docs: create時のドキュメントを実装

--- routes/v0/rooms/create.ts
+++ routes/v0/rooms/create.ts
@@ -5,6 +5,39 @@ import { createRoom } from '../../../model/createRoom';
 
 export const router = Router();
 
+/**
+ *  @swagger
+ *  /api/v0/rooms/create:
+ *    post:
+ *      description: Create new room data in DB, and return roomId, name and createdAt. This creator will be joined to the created room data.
+ *      parameters:
+ *      - in: header
+ *        name: Authorization
+ *        type: string
+ *        required: true
+ *      - in: body
+ *        name: room name
+ *        schema:
+ *          type: object
+ *          properties:
+ *            name:
+ *              type: string
+ *      responses:
+ *        200:
+ *          description: Create new room data in DB, and return roomId, name and createdAt.
+ *          schema:
+ *            type: object
+ *            properties:
+ *              roomId:
+ *                type: string
+ *                format: uuid
+ *              name:
+ *                type: string
+ *              createdAt:
+ *                type: string
+ *                format: date-time
+ *
+ */
 router.post(
   '/rooms/create',
   passport.authenticate('bearer', { session: false }),
