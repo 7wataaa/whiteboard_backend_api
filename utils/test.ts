00b50a175e325380e2638ac147c3e680e4067582
nashi <nn.contact.n@gmail.com>
2021-10-20 10:26:18 +0900

    test: トークン有効期限切れのテストを実装

--- utils/test.ts
+++ utils/test.ts
@@ -12,6 +12,7 @@ const iso8601RegExp = /Z|[+-](0\d|1[012])(:?[012345]\d)?/;
 
 afterEach(async () => {
   jest.restoreAllMocks();
+  jest.useRealTimers();
 
   await prisma.room.deleteMany();
   await prisma.token.deleteMany();
@@ -383,6 +384,29 @@ describe('/api/v0/users/me', () => {
     const response = await request(app).get('/api/v0/users/me');
 
     expect(response.status).toBe(401);
+  });
+
+  test('有効期限切れのトークンでgetしたときのテスト', async () => {
+    const tokenExpirationTestEmail = 'tokenexpirationtest@example.com';
+    const tokenExpirationTestPass = 'password';
+
+    const user = await User.createUserByEmailAndPassword(
+      tokenExpirationTestEmail,
+      tokenExpirationTestPass,
+      ''
+    );
+
+    const after1hourDate = new Date(
+      new Date().setHours(new Date().getHours() + 1)
+    );
+
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(after1hourDate);
+
+    const response = await request(app)
+      .get('/api/v0/users/me')
+      .auth(user.validToken.loginToken, { type: 'bearer' })
+      .expect(401);
   });
 });
 
