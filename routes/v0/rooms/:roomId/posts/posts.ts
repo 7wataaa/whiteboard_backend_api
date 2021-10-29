1d48bb9ac410e1f9705e223746dbceaa9127076c
nashi <nn.contact.n@gmail.com>
2021-10-29 14:38:10 +0900

    add: 投稿の一覧取得機能を実装

--- routes/v0/rooms/:roomId/posts/posts.ts
+++ routes/v0/rooms/:roomId/posts/posts.ts
@@ -10,63 +10,123 @@ import { User } from '../../../../../model/user';
 
 export const router = Router();
 
-// 投稿の新規作成
-router.post(
-  '/rooms/:roomId/posts',
-  passport.authenticate('bearer', { session: false }),
-  async (req: Request, res: Response) => {
-    const isUser = (user: any): user is User =>
-      user.id != null && user.createdAt != null;
+const isUser = (user: any): user is User =>
+  user.id != null && user.createdAt != null;
 
-    if (!isUser(req.user)) {
-      res.sendStatus(500);
-      return;
-    }
+router
+  .post(
+    // 投稿の新規作成
+    '/rooms/:roomId/posts',
+    passport.authenticate('bearer', { session: false }),
+    async (req: Request, res: Response) => {
+      // パラメーターが正常かの判定と、Roomの取得
 
-    const roomId = req.params['roomId'];
-
-    console.log(roomId);
-
-    if (!roomId || ![32, 36].includes(roomId.length)) {
-      res.sendStatus(400);
-      return;
-    }
-
-    const room = await Room.findRoomById(roomId).catch((e) => {
-      if (e instanceof RoomNotFoundError) {
-        return null;
-      } else {
-        throw e;
+      if (!isUser(req.user)) {
+        res.sendStatus(500);
+        return;
       }
-    });
 
-    if (!room) {
-      res.sendStatus(400);
-      return;
-    }
+      const roomId = req.params['roomId'];
 
-    if (!req.user.isInRoom(room)) {
-      res.sendStatus(400);
-      return;
-    }
+      if (!roomId || ![32, 36].includes(roomId.length)) {
+        res.sendStatus(400);
+        return;
+      }
 
-    const postId = await room
-      .createNewPost(new Post({ text: req.body['text'], author: req.user }))
-      .catch((e) => {
-        if (e instanceof EmptyTextPostError) {
+      const room = await Room.findRoomById(roomId).catch((e) => {
+        if (e instanceof RoomNotFoundError) {
           return null;
         } else {
           throw e;
         }
       });
 
-    if (!postId) {
-      res.sendStatus(400);
-      return;
-    }
+      if (!room) {
+        res.sendStatus(400);
+        return;
+      }
 
-    res.status(200).json({
-      id: postId,
-    });
-  }
-);
+      if (!req.user.isInRoom(room)) {
+        res.sendStatus(400);
+        return;
+      }
+
+      // パラメーターが正常なので投稿を作成
+
+      const postId = await room
+        .createNewPost(new Post({ text: req.body['text'], author: req.user }))
+        .catch((e) => {
+          if (e instanceof EmptyTextPostError) {
+            return null;
+          } else {
+            throw e;
+          }
+        });
+
+      if (!postId) {
+        res.sendStatus(400);
+        return;
+      }
+
+      res.status(200).json({
+        id: postId,
+      });
+    }
+  )
+  .get(
+    // この部屋の投稿を取得
+    '/rooms/:roomId/posts',
+    passport.authenticate('bearer', { session: false }),
+    async (req: Request, res: Response) => {
+      // パラメーターが正常かの判定と、Roomの取得
+
+      if (!isUser(req.user)) {
+        res.sendStatus(500);
+        return;
+      }
+
+      const roomId = req.params['roomId'];
+
+      if (!roomId || ![32, 36].includes(roomId.length)) {
+        res.sendStatus(400);
+        return;
+      }
+
+      const room = await Room.findRoomById(roomId).catch((e) => {
+        if (e instanceof RoomNotFoundError) {
+          return null;
+        } else {
+          throw e;
+        }
+      });
+
+      if (!room) {
+        res.sendStatus(400);
+        return;
+      }
+
+      if (!req.user.isInRoom(room)) {
+        res.sendStatus(400);
+        return;
+      }
+
+      // パラメーターが正常なので投稿一覧を返す
+
+      const posts = await room.fetchAllPosts().catch((e) => {
+        if (e instanceof RoomNotFoundError) {
+          return null;
+        } else {
+          throw e;
+        }
+      });
+
+      if (!posts) {
+        res.sendStatus(400);
+        return;
+      }
+
+      res.status(200).json({
+        posts: posts,
+      });
+    }
+  );
